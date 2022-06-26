4f36ac1b661b5696d04823099a5b5dbeb2972f28
shivansh <shivansh@cognitivegarage.ai>
2022-06-26 07:27:55 +0530

    working now

--- src/lower-case.ts
+++ src/lower-case.ts
@@ -1,31 +1,23 @@
 /**** some basic type definitions for safety (or for illustration) ****/
 
-type NR_Node      = any
+import { Node, NodeAPI, NodeDef } from 'node-red'
 
-import { Node, NodeAPI, NodeDef, NodeMessage, NodeMessageInFlow } from 'node-red'
+  module.exports = function (RED:NodeAPI):void {
+    
+    function LowerCaseNode(this:Node, config:NodeDef):void {
 
+      RED.nodes.createNode(this,config);
 
-/**** actual node definition ****/
+      this.on('input', function(msg, send, done) {
+        if(typeof(msg.payload) ==  "string"){
+          msg.payload = msg.payload.toLowerCase();
+        }
+        send(msg);
+        done();
+      });
 
-  export default function (RED:NodeAPI):void {
-    let NodeSet:{ [Id:string]:NR_Node }        // will be filled at flow startup
+    }
 
-    let InvocationCounter:number                    = 0
-    let InvocationMapper:{ [Index:number]:NR_Node } = Object.create(null)
-
-  /**** resuable-in ****/
-
-  function LowerCaseNode(this:Node, config:NodeDef):void {
-
-    RED.nodes.createNode(this,config);
-
-    this.on('input', function(msg, send, done) {
-      if(typeof(msg.payload) ==  "string"){
-        msg.payload = msg.payload.toLowerCase();
-      }
-      send(msg);
-      done();
-    });
-  }
-  
+    RED.nodes.registerType("lower-case", LowerCaseNode);
+    
   }
